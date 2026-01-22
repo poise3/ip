1db6c7c6ecebc69f718c92ed16b867659dc1c901
Jin Hui <105375844+poise3@users.noreply.github.com>
2026-01-22 18:54:28 +0800

    Level-1

--- src/main/java/Mobi.java
+++ src/main/java/Mobi.java
@@ -1,10 +1,23 @@
+import java.util.Scanner;
+
 public class Mobi
 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("____________________________________________________________\n" +
                 " Hello! I'm Mobi\n" +
                 " What can I do for you?\n" +
-                "____________________________________________________________\n" +
+                "____________________________________________________________\n");
+
+        String userInput = scanner.nextLine();
+        while (!userInput.equals("bye")) {
+            System.out.println("____________________________________________________________\n" +
+                     userInput + "\n" +
+                    "____________________________________________________________\n");
+            userInput = scanner.nextLine();
+        }
+        System.out.println("____________________________________________________________\n" +
                 " Bye. Hope to see you again soon!\n" +
                 "____________________________________________________________\n");
     }
