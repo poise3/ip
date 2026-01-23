418ad3fa6451867d58d8377835170bc8ef4b1201
Jin Hui <105375844+poise3@users.noreply.github.com>
2026-01-23 15:01:51 +0800

    Implemented handlers for each command

--- src/main/java/Mobi.java
+++ src/main/java/Mobi.java
@@ -2,9 +2,10 @@ import java.util.Scanner;
 
 public class Mobi
 {
+    private static final Task[] taskList = new Task[100];
+    private static int count = 0;
+
     public static void main(String[] args) {
-        Task[] taskList = new Task[100];
-        int count = 0;
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("____________________________________________________________\n" +
@@ -20,41 +21,22 @@ public class Mobi
 
             switch (command) {
                 case "list":
-                    System.out.println("Here are the tasks in your list:");
-                    for (int i = 0; i < count; i++) {
-                        System.out.println((i + 1) + "." + taskList[i].toString());
-                    }
+                    listHandler();
                     break;
                 case "mark":
-                    int num = Integer.parseInt(inputs[1]);
-                    taskList[num - 1].markAsDone();
+                    markHandler(inputs[1]);
                     break;
                 case "unmark":
-                    int num2 = Integer.parseInt(inputs[1]);
-                    taskList[num2 - 1].markNotDone();
+                    unmarkHandler(inputs[1]);
                     break;
                 case "todo":
-                    System.out.println("Got it. I've added this task: ");
-                    taskList[count] = new Todo(inputs[1]);
-                    System.out.println(taskList[count].toString());
-                    count++;
-                    System.out.println("Now you have " + count + " tasks in the list.");
+                    todoHandler(inputs[1]);
                     break;
                 case "deadline":
-                    String[] parts = inputs[1].trim().split("/by");
-                    System.out.println("Got it. I've added this task: ");
-                    taskList[count] = new Deadline(parts[0].trim(), parts[1].trim());
-                    System.out.println(taskList[count].toString());
-                    count++;
-                    System.out.println("Now you have " + count + " tasks in the list.");
+                    deadlineHandler(inputs[1]);
                     break;
                 case "event":
-                    String[] parts2 = inputs[1].trim().split("(/from|/to)");
-                    System.out.println("Got it. I've added this task: ");
-                    taskList[count] = new Event(parts2[0].trim(), parts2[1].trim(), parts2[2].trim());
-                    System.out.println(taskList[count].toString());
-                    count++;
-                    System.out.println("Now you have " + count + " tasks in the list.");
+                    eventHandler(inputs[1]);
                     break;
                 default:
                     System.out.println("please enter valid command");
@@ -67,5 +49,48 @@ public class Mobi
         System.out.println("____________________________________________________________\n" +
                 " Bye. Hope to see you again soon!\n" +
                 "____________________________________________________________\n");
+    }
+
+    private static void listHandler() {
+        System.out.println("Here are the tasks in your list:");
+        for (int i = 0; i < count; i++) {
+            System.out.println((i + 1) + "." + taskList[i].toString());
+        }
+    }
+
+    private static void markHandler(String number) {
+        int num = Integer.parseInt(number);
+        taskList[num - 1].markAsDone();
+    }
+
+    private static void unmarkHandler(String number) {
+        int num = Integer.parseInt(number);
+        taskList[num - 1].markNotDone();
+    }
+
+    private static void todoHandler(String task) {
+        System.out.println("Got it. I've added this task: ");
+        taskList[count] = new Todo(task);
+        System.out.println(taskList[count].toString());
+        count++;
+        System.out.println("Now you have " + count + " tasks in the list.");
+    }
+
+    private static void deadlineHandler(String task) {
+        String[] parts = task.trim().split("/by");
+        System.out.println("Got it. I've added this task: ");
+        taskList[count] = new Deadline(parts[0].trim(), parts[1].trim());
+        System.out.println(taskList[count].toString());
+        count++;
+        System.out.println("Now you have " + count + " tasks in the list.");
+    }
+
+    private static void eventHandler(String task) {
+        String[] parts2 = task.trim().split("(/from|/to)");
+        System.out.println("Got it. I've added this task: ");
+        taskList[count] = new Event(parts2[0].trim(), parts2[1].trim(), parts2[2].trim());
+        System.out.println(taskList[count].toString());
+        count++;
+        System.out.println("Now you have " + count + " tasks in the list.");
     }
 }
