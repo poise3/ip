b2978aa4549367b63156f712a359f2d76fce7d6f
Jin Hui <105375844+poise3@users.noreply.github.com>
2026-02-02 18:06:40 +0800

    Add FindCommand JUnit test

--- src/test/java/mobi/command/FindCommandTest.java
+++ src/test/java/mobi/command/FindCommandTest.java
@@ -1,4 +1,28 @@
 package mobi.command;
 
+import mobi.task.Task;
+import mobi.task.TaskList;
+import mobi.task.Todo;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class FindCommandTest {
+    @Test
+    public void execute_ValidInput_findsTasks(){
+        TaskList taskList = new TaskList();
+        Task task = new Todo("b");
+        Task task2 = new Todo("a 2");
+        Task task3 = new Todo("2 ab");
+        String find = "a";
+        taskList.add(task);
+        taskList.add(task2);
+        taskList.add(task3);
+
+        assertFalse(taskList.get(0).getDesc().contains(find));
+        assertTrue(taskList.get(1).getDesc().contains(find));
+        assertTrue(taskList.get(2).getDesc().contains(find));
+
+    }
 }
